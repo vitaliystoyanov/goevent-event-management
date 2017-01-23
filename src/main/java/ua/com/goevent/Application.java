fd6b2382a95b401f63519586c004bdb83e846047
loganstk <logan.zhi@gmail.com>
2017-01-23 22:18:42 +0200

    a test 2

--- src/main/java/ua/com/goevent/Application.java
+++ src/main/java/ua/com/goevent/Application.java
@@ -2,11 +2,9 @@ package ua.com.goevent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ua.com.goevent.web.HomeController;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
