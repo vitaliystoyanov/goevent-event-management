3e4e8e60a38324d1ad166fbbb33a4582410ed197
Vitaliy <developer.stoyanov@gmail.com>
2017-01-30 00:42:41 +0200

    upgrading views

--- src/main/java/ua/com/goevent/web/HomeController.java
+++ src/main/java/ua/com/goevent/web/HomeController.java
@@ -5,6 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @RequestMapping("/event")
+    public String event() {
+        return "event";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @RequestMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
     @RequestMapping("/")
     public String index() {
         return "index";
